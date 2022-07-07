93de7133584cb1795ebc0b8b5e67e5084478d268
Chreees79 <christelle.gevaert32@gmail.com>
2022-07-07 14:03:00 +0200

    add case file

---
+++ lib/00_journalists.rb
@@ -0,0 +1,186 @@
+
+def array
+  journalists = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+end
+
+
+# Combien y a-t-il de handle dans cette array ?
+
+def handle_number(journalists)
+  puts "Combien y a-t-il de journalistes dans ce array ?"
+  puts ""
+  puts "**********************************************************"
+  puts ""
+  sleep 2
+  puts  " il y a quand même #{journalists.size} journalistes dans cet array, ça en fait ! les news seront fraîches!!"
+end
+
+# Quel est le handle le plus court de cette liste ?
+
+def handle_short_size(journalists)
+  puts "Quel est le handle le plus court de cette liste ?"
+  puts ""
+  puts "**********************************************************"
+  puts ""
+  sleep 2
+  puts "Comme son nom l'indique, #{journalists.sort_by!(&:size)[0]} est le plus court, visionnaire le Monsieur !"
+
+end
+
+# Combien y-a-t'il de handle contenant 5 caractères (le @ ne compte pas pour un caractère)
+
+def five_caracters (journalists)
+  puts "Combien y a-t-il de handle contenant 5 caractères (le @ ne compte pas pour un caractère) ?"
+  puts ""
+  puts "**********************************************************"
+  puts ""
+  sleep 2
+  number = 0
+  journalists.each {|journalist| number += 1 if journalist.size == 6}
+  puts "il y a #{number} journalistes qui ont 5 caractères après le @ "
+
+end
+
+
+ # Combien commencent par une majuscule (première lettre juste après le @) ?
+
+def upercase_handle(journalists)
+  puts "Combien commencent par une majuscule (première lettre juste après le @) ?"
+  puts ""
+  puts "**********************************************************"
+  puts ""
+  sleep 2
+  if journalists.any? {|journalist| journalist[1] == journalist[1].upcase}
+    number = journalists.count {|journalist| journalist[1] == journalist[1].upcase}
+    puts "il y a #{number} journalistes qui commencent par une majuscule"
+  end
+end
+
+ # Trie la liste de handle par ordre alphabétique.
+
+def order_array(journalists)
+  puts "Triez cette liste de handle par ordre alphabétique ? et bien la voici !"
+  puts ""
+  puts "**********************************************************"
+  puts ""
+  sleep 2
+  puts journalists.sort_by!(&:downcase)
+end
+
+# Trie la liste de handle par taille des handle (les plus petits en premiers, les plus grands après)
+
+def list_size_array(journalists)
+  puts "Trie la liste de handle par taille des handle (les plus petits en premiers, les plus grands après)"
+  puts ""
+  puts "**********************************************************"
+  puts ""
+  sleep 2
+  puts journalists.sort_by!(&:length)
+end
+
+ # Quelle est la position dans l'array de la personne @epenser ?
+
+def search_index(journalists)
+  puts "Quelle est la position dans l'array de @epenser ?"
+  puts ""
+  puts "**********************************************************"
+  puts ""
+  sleep 2
+  puts journalists.index("@epenser")
+end
+
+# Sors-moi une répartition des handle par taille de ces derniers (nombre de handle avec 1 caractère, nombre de handle avec 2 caractères, nombre de handle avec 3 caractères, etc)
+
+def distribution_size_handle(journalists)
+  puts "Sors-moi une répartition des handle par taille de ces derniers (nombre de handle avec 1 caractère, nombre de handle avec 2 caractères, nombre de handle avec 3 caractères, etc)"
+  puts ""
+  puts "**********************************************************"
+  puts ""
+  sleep 2
+  tab_1 = journalists.group_by {|journalist| journalist.size}.sort
+  tab_1.each do |key, value|
+  puts "il y a #{(value.size)} handle dont la longueur est de #{key} caractères ! "
+  end
+end
+
+def menu
+  puts "  🌟🌟🌟🌟🌟🌟🌟🌟🌟🌟🌟🌟🌟🌟🌟   BIENVENUE DANS LE JEU DE QUESTIONS/REPONSES    🌟🌟🌟🌟🌟🌟🌟🌟🌟🌟🌟🌟🌟🌟🌟🌟🌟🌟?"
+  sleep 1
+  puts ""
+  puts " -----------------------------------      A vous de choisir !!     -----------------------------------------"
+  sleep 1
+  puts ""
+  puts "                         1 - Afficher la liste de tous les journalistes"
+  puts "                         2 - Afficher le handle le plus court"
+  puts "                         3 - Combien y a-t-il de handle contenant 5 caractères ?"
+  puts "                         4 - Combien commencent par une majuscule ?"
+  puts "                         5 - Trie la liste de handle par ordre alphabétique"
+  puts "                         6 - Trie la liste de handle par taille des handle"
+  puts "                         7 - Quelle est la position dans l'array de la personne @epenser ?"
+  puts "                         8 - Sors-moi une répartition des handle par taille de ces derniers "
+  puts "                         9 - Quitter le programme"
+  puts "                         A vous de choisir entre 1 et 9 !!"
+  puts ""
+  print "                        > "
+  user_choice = gets.chomp.to_i
+end
+
+def next_
+  puts
+  puts
+  puts"                  ------------------------------------------------------------"
+  puts "                |          Appuyez sur Entrer pour revenir au menu.          |"
+  puts "                 ------------------------------------------------------------"                                                         
+  gets
+  system "clear"
+end
+
+def perform
+  journalists = array
+	case menu
+	when 1
+    system "clear"
+		handle_number(journalists)
+    next_
+    perform
+	when 2
+    system "clear"
+		handle_short_size(journalists)
+    next_
+		perform
+	when 3
+    system "clear"
+		five_caracters(journalists)
+    next_
+		perform
+  when 4
+    system "clear"
+		upercase_handle(journalists)
+    next_
+		perform
+  when 5
+    system "clear"
+		order_array(journalists)
+    next_
+		perform
+  when 6
+    system "clear"
+		list_size_array(journalists)
+    next_
+		perform
+  when 7
+    system "clear"
+		search_index(journalists)
+    next_
+		perform
+  when 8
+    system "clear"
+		distribution_size_handle(journalists)
+    next_
+		perform
+  when 9
+    system "clear"
+	end
+end
+
+perform
